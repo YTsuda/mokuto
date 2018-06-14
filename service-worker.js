da726beaf94c7b53fabe119ac23d5349b5ff0d9b
Yasuyuki Tsuda <tsuda@howtelevision.jp>
2018-06-14 13:28:24 +0900

    enable cache

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,42 @@
+var cacheName = 'PWA_1';
+var filesToCache = [
+  '/',
+  '/index.html'
+];
+
+
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
+
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
 });
 
 // 現状では、この処理を書かないとService Workerが有効と判定されないようです
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('fetch', function(event) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
